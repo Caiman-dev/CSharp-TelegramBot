01ee94ca8195be835475d632a8c5ab324199b6f0
Иванов Александр <ottooctavius7777@gmail.com>
2023-04-09 18:59:02 +0300

    Initial commit

---
+++ TelegramBot/TelegramBot/Configuration.cs
@@ -0,0 +1,9 @@
+namespace TelegramBot
+{ 
+
+public static class Configuration
+{
+    public readonly static string BotToken = "5390747319:AAEsj4pqGCAtF0S5nLsnggxfwQveHXXiVEg";
+}
+
+}
---
+++ TelegramBot/TelegramBot/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Extensions.Polling;
+using Telegram.Bot.Types;
+
+namespace TelegramBot
+{
+
+    public static class Program
+    {
+        private static TelegramBotClient? Bot;
+
+        public static async Task Main()
+        {
+            Bot = new TelegramBotClient(Configuration.BotToken);
+
+            User me = await Bot.GetMeAsync();
+            Console.Title = me.Username ?? "My awesome Bot";
+
+            using var cts = new CancellationTokenSource();
+
+            // StartReceiving does not block the caller thread. Receiving is done on the ThreadPool.
+            ReceiverOptions receiverOptions = new() { AllowedUpdates = { } };
+            Bot.StartReceiving(Handlers.HandleUpdateAsync,
+                               Handlers.HandleErrorAsync,
+                               receiverOptions,
+                               cts.Token);
+
+            Console.WriteLine($"Start listening for @{me.Username}");
+            Console.ReadLine();
+
+            // Send cancellation request to stop bot
+            cts.Cancel();
+        }
+    }
+}
